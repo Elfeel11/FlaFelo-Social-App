6992356362daf313a49fb9272be7e27f1fea44dd
Ahmed Mohamed Elfeel <ahmedelfeel210@gmail.com>
2026-07-12 14:22:17 +0300

    Refactor imports for HeroUIProvider and ToastProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import {HeroUIProvider} from "@heroui/react";
-import {ToastProvider} from "@heroui/toast";
+import {HeroUIProvider,ToastProvider} from "@heroui/react";
+// import {ToastProvider} from "@heroui/toast";
 import AuthContextProvider from './Contexets/authContext';
 
 
